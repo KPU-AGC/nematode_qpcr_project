c97faf3a25ed110d48c89a901fef0cf408447099
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-05-13 15:50:52 -0700

    Added output argument

--- qpcrdesign/probesearch.py
+++ qpcrdesign/probesearch.py
@@ -404,12 +404,24 @@ def get_target_accessions(path):
 
 def parse_args(): 
     parser = argparse.ArgumentParser(description='probesearch.py - identify viable probes in an alignment for given target sequences')
-    parser.add_argument('target_alignment_path', 
+    parser.add_argument(
+        'target_alignment_path', 
         action='store', 
         type=pathlib.Path,
         help = 'Path to target alignment file, fasta format'
     )
-    parser.add_argument('--target_start',
+    parser.add_argument(
+        '--output', 
+        '-output',
+        metavar='output_directory',
+        dest='output_path',
+        default=None,
+        action='store',
+        type=pathlib.Path, 
+        help='Output path',
+    )
+    parser.add_argument(
+        '--target_start',
         metavar='target_start', 
         dest='target_start',
         default=1,
@@ -417,7 +429,8 @@ def parse_args():
         type=int,
         help='Start coordinate of target region, 1-based coordinates'
     )
-    parser.add_argument('--target_end',
+    parser.add_argument(
+        '--target_end',
         metavar='target_end', 
         dest='target_end', 
         default=None,
@@ -425,40 +438,46 @@ def parse_args():
         type=int,
         help='End coordinate of target region, 1-based coordinates'
     )
-    parser.add_argument('--min_primer_len',
+    parser.add_argument(
+        '--min_primer_len',
         action='store',
         type=int, 
         default=17,
         dest='min_primer_len',
         help='Minimum primer length (default=17)'
     )
-    parser.add_argument('--max_primer_len',
+    parser.add_argument(
+        '--max_primer_len',
         action='store',
         type=int, 
         default=22,
         dest='max_primer_len',
         help='Maximum primer length (default=22)'
     )
-    parser.add_argument('--no_sens_spec_check',
+    parser.add_argument(
+        '--no_sens_spec_check',
         action='store_true',
         dest='sens_spec_flag',
         help='Flag to not check the putative probes for their specificity and sensitivity'
     )
     #Arguments for specificity checking
-    parser.add_argument('--blastdb',
+    parser.add_argument(
+        '--blastdb',
         action='store',
         type=str,
         dest='blastdb',
         default='',
         help='Name of blastdb'
     )
-    parser.add_argument('--blastdb_len',
+    parser.add_argument(
+        '--blastdb_len',
         action='store',
         type=int,
         dest='blastdb_len',
         help='Length of blastdb'
     )
-    parser.add_argument('--target_accessions', 
+    parser.add_argument(
+        '--target_accessions', 
         metavar='target_accessions', 
         action='store', 
         type=pathlib.Path,
@@ -467,13 +486,15 @@ def parse_args():
         help = 'Path to target_accessions'
     )
     args = parser.parse_args()
+    if args.output_path is None: 
+        args.output_path = pathlib.Path(args.target_alignment_path.parent)
     #Argument order: target sequence, target start coordinate, target end coordinate, minimum primer length, max primer length, check flag, blastdb, target accession path
     #Note that coordinates are converted to 0-based half-open coordinates
-    return (args.target_alignment_path, args.target_start-1, args.target_end, args.min_primer_len, args.max_primer_len, args.sens_spec_flag, args.blastdb, args.blastdb_len, args.target_accessions)
+    return (args.target_alignment_path, args.output_path, args.target_start-1, args.target_end, args.min_primer_len, args.max_primer_len, args.sens_spec_flag, args.blastdb, args.blastdb_len, args.target_accessions)
 
 def main():
     #Arguments
-    target_alignment_path, target_start, target_end, min_primer_len, max_primer_len, check_flag, blastdb, blastdb_len, target_accession_path = parse_args()
+    target_alignment_path, output_path, target_start, target_end, min_primer_len, max_primer_len, check_flag, blastdb, blastdb_len, target_accession_path = parse_args()
     #Process the alignment
     target_alignment = alignment(target_alignment_path)
     target_consensus = target_alignment.get_consensus()
@@ -493,11 +514,11 @@ def main():
             probe.calculate_specificity(blast_results[probe.id], target_accessions, blastdb_len)
             probe.calculate_score()
         #Output BLAST results
-        pb_blast.output(blast_results, target_alignment_path)
+        pb_blast.output(blast_results, output_path)
         #Output probe list
-        pb_gen.output(target_alignment_path)
+        pb_gen.output(output_path)
     else: 
-        pb_gen.output(target_alignment_path)
+        pb_gen.output(output_path)
 
 if __name__ == '__main__': 
     main()
