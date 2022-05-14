50ac804711c1d54c9a7be260e889374aa645457e
Michael Ke <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-05-13 17:14:08 -0700

    actually fixed output path

--- qpcrdesign/probesearch.py
+++ qpcrdesign/probesearch.py
@@ -393,7 +393,7 @@ class nemaBlast:
 
     def output(self, blast_results, path): 
         #Make path to store all of the blast results
-        blast_folder_path = path.with_name('probe_blast')
+        blast_folder_path = path.joinpath('probe_blast')
         blast_folder_path.mkdir(exist_ok=True)
         #Go through blast result dictionary and output all of the data
         for blast_result_key in blast_results: 
